7645f39e446bdf5f762b29a175c51491c14bd00b
yourUsername <yourEmail@domain.com>
2020-02-10 16:48:17 -0500

    bb

---
+++ src/hoax.java
@@ -0,0 +1,17 @@
+public class hoax {
+
+public static void main(String[]args){
+
+ String[] []  names= {{"alemu", "ayele","abebe"},{"askal", "akoye"}};
+
+    System.out.println( ""+ names[0][0]+":"+ names[1][1] );
+
+
+
+
+
+
+
+}
+
+}
